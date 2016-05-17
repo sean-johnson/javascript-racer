7058f8db0b55895535752008991cf673932c4d46
Charlotte Smith <hello@seanjohnson.nz>
2016-05-17 13:47:59 +1200

    trying to merge from feature branch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,77 +5,19 @@ document.addEventListener('keyup', function(keyPress) {
 var letter = String.fromCharCode(event.keyCode).toLowerCase();
 
   if (letter === 'z') {
-      player1Punch();
+      playerPunch("player1-arm", 0, "hookRight", "pow1", "p1-score", "skin", "fist", player1Score);
     } else if (letter === 'm') {
-      playerPunch("player2-arm", 1, "hookLeft", "pow2", "p2-score", "skin2", "fist2");
+      playerPunch("player2-arm", 1, "hookLeft", "pow2", "p2-score", "skin2", "fist2", player2Score);
     } else {
       alert("Press 'z' to punch as Zombie or press 'm' to punch as Batman");
     }
   
 });
 
-var playerScore = 0;
+var player1Score = 0
+var player2Score = 0;
 
-// Player One Punch Function
-function player1Punch(){
-
-	var fist = document.getElementById("player1-arm").querySelectorAll("td");
-	
-	//Variables for new cells that extend arm
-	var row = document.querySelectorAll("tr")[0];
-	var extendArm = row.insertCell(-1);
-	var armLength = document.getElementById("player1-arm").querySelectorAll("td").length;
-
-	//Keep 'fist' as the last cell only, remove class for previous cell
-	var arm = fist[fist.length-1];
-	var punchHit = fist[fist.length];
-
-
-	//If fist reaches face: play and show sound effect, congratulate Player One, replay?,  otherwise: Keep 'fist' as the last cell only, remove class for previous cell, keep extending arm.
-	if (armLength >= 7) {
-
-		var rightHook = document.getElementById("hookRight");
-		rightHook.play(); 
-
-		var pow1 = document.getElementById("pow1");
-		pow1.style.visibility = 'visible';
-		extendArm.className = "hidden";
-
-		alert("Batman got knocked Dafuqout!!");
-
-		// Add count to playerOneScore
-		playerOneScore += 1;
-		document.getElementById("p1-score").innerHTML = playerOneScore;
-
-		// Replay for another round
-		function playAgain1() {
-
-			if (confirm('Another round?')) {
-
-				for (var i = 0; i < 6; i++) {
-					extendArm = row.deleteCell(0);
-					arm = fist[fist.length-1];
-					punchHit = fist[fist.length];
-				}
-
-				//Take out POW effect
-				pow1.style.visibility = 'hidden';
-
-				//Important reset in next line
-				player1Punch();
-			}  
-		}
-
-		playAgain1();
-
-	} else {
-		extendArm.className = " " + "fist";
-		arm.className = "skin";
-	}
-    
-}
-
-function playerPunch(player, rowNum, sound, effect, score, skin, fist) {
+function playerPunch(player, rowNum, sound, effect, score, skin, fistType, playerScoreNum) {
 
 	var fist = document.getElementById(player).querySelectorAll("td");
 	//Create new cells to extend arm
@@ -87,7 +29,7 @@ function playerPunch(player, rowNum, sound, effect, score, skin, fist) {
 	var arm = fist[fist.length-1];
 	var punchHit = fist[fist.length-1];
 
-	//If fist reaches face: play and show sound effect, congratulate Player Two, replay?,  otherwise: Keep 'fist' as the last cell only, remove class for previous cell, keep extending arm.
+	//If fist reaches face: play and show sound effect, congratulate Player X, replay?,  otherwise: Keep 'fist' as the last cell only, remove class for previous cell, keep extending arm.
 	if (armLength >= 7) {
 
 		var hook = document.getElementById(sound);
@@ -96,98 +38,73 @@ function playerPunch(player, rowNum, sound, effect, score, skin, fist) {
 		var pow = document.getElementById(effect);
 		pow.style.visibility = 'visible';
 		extendArm.className = "hidden"; 
-		alert("You Won!!");
 
-		// Add count to playerOneScore
-		playerScore += 1;
-		document.getElementById(score).innerHTML = playerScore;
+		if (player === "player1-arm") { 
 
-		// Replay for another round
-		function playAgain() {
+			alert ("Zombie, You Won!!");
+			// Add count to player1Score
+			player1Score += 1;
+			document.getElementById(score).innerHTML = player1Score;
 
-			if (confirm('Another round?')) {
+		} else if (player === "player2-arm") {
 
-				for (var i = 0; i < 6; i++) {
-					extendArm = row.deleteCell(0);
-					arm = fist[fist.length-1];
-					punchHit = fist[fist.length];
-				}
+			alert ("Batman, You Won!!");
+			// Add count to player2Score
+			player2Score += 1;
+			document.getElementById(score).innerHTML = player2Score;
 
-				//Take out POW effect
-				pow.style.visibility = 'hidden';
+		} else {}
 
-				//Important reset in next line
-				playerPunch();
-			}  
-		}
+		//Take out 'POW' graphic
+		pow.style.visibility = 'hidden';
+
 
 		playAgain();
 
+
 	} else {
-		extendArm.className = " " + "fist2";
+
 		arm.className = skin;
+		extendArm.className = fistType;
+		
+
 	}
 }
 
-/*
-
-// Player Two Punch Function
-function player2Punch(){
-
-	var fist = document.getElementById("player2-arm").querySelectorAll("td");
-	
-	//Create new cells to extend arm
-	var row = document.querySelectorAll("tr")[1];
-	var extendArm = row.insertCell(-1);
-	var armLength = document.getElementById("player2-arm").querySelectorAll("td").length;
-
-	//Keep 'fist' as the last cell only, remove class for previous cell
-	var arm = fist[fist.length-1];
-	var punchHit = fist[fist.length-1];
 
 
-	//If fist reaches face: play and show sound effect, congratulate Player Two, replay?,  otherwise: Keep 'fist' as the last cell only, remove class for previous cell, keep extending arm.
-	if (armLength >= 7) {
+// Replay for another round
+function playAgain() {
 
-		var leftHook = document.getElementById("hookLeft");
-		leftHook.play(); 
- 
-		var pow2 = document.getElementById("pow2");
-		pow2.style.visibility = 'visible';
-		extendArm.className = "hidden"; 
-		alert("Zombie ded!! NanananananaBatman!");
+	if (confirm('Another round?')) {
+		
+		var parent = document.getElementById("player1-arm");
 
-		// Add count to playerOneScore
-		playerTwoScore += 1;
-		document.getElementById("p2-score").innerHTML = playerTwoScore;
+			var elementChildren = parent.children;
 
-		// Replay for another round
-		function playAgain2() {
+			for (var i = 0; elementChildren.length > 1; i++) {
 
-			if (confirm('Another round?')) {
+				parent.removeChild(parent.firstChild);
 
-				for (var i = 0; i < 6; i++) {
-					extendArm = row.deleteCell(0);
-					arm = fist[fist.length-1];
-					punchHit = fist[fist.length];
-				}
+			}
 
-				//Take out POW effect
-				pow2.style.visibility = 'hidden';
+		var parent2 = document.getElementById("player2-arm");
 
-				//Important reset in next line
-				player2Punch();
-			}  
-		}
+			var elementChildren = parent2.children;
 
-		playAgain2();
-	} else {
-		extendArm.className = " " + "fist2";
-		arm.className = "skin2";
-	}
-    
+			for (var i = 0; elementChildren.length > 1; i++) {
+
+				parent2.removeChild(parent2.firstChild);
+
+			}
+		
+
+	}  else {}
+
+
 }
 
-*/
 
 
+
+		
